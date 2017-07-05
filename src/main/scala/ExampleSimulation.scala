910700c3fdf07fca9aac589336a1e808a68a7296
Paweł Wacławczyk <p.waclawczyk@clearcode.cc>
2017-07-05 13:43:01 +0200

    Ask for simulation parameters.

--- src/main/scala/ExampleSimulation.scala
+++ src/main/scala/ExampleSimulation.scala
@@ -1,20 +1,43 @@
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 import scala.concurrent.duration._
+import scala.io.StdIn
 
 class ExampleSimulation extends Simulation {
-  val httpConf = http
-      .baseURL("http://computer-database.gatling.io")
+
+  println("Base URL:")
+  val baseUrl: String = StdIn.readLine() match {
+    case "" => "http://127.0.0.1:9000"
+    case url => url
+  }
+
+  println("Duration in seconds:")
+  val duration: Int = StdIn.readLine() match {
+    case "" => 10
+    case s => s.toInt
+  }
+
+  println("Users per second:")
+  val usersPerSecond: Int = StdIn.readLine() match {
+    case "" => 10
+    case s => s.toInt
+  }
+
+  val httpConf: HttpProtocolBuilder = http
+      .baseURL(baseUrl)
       .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
       .doNotTrackHeader("1")
       .acceptLanguageHeader("en-US,en;q=0.5")
       .acceptEncodingHeader("gzip, deflate")
       .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("Basic")
-      .exec(http("request").get("/"))
+  val scn: ScenarioBuilder = scenario("Basic")
+      .exec(http("request").get("/pixel.gif"))
       .pause(5)
 
-  setUp(scn.inject(atOnceUsers(1)))
-      .protocols(httpConf)
+  setUp(scn.inject(
+    constantUsersPerSec(usersPerSecond) during(duration seconds)
+  )).protocols(httpConf)
 }
